2642631d4324c4c6c63d61e566495812229289e6
Gabriele Virga <Gabriele_Virga@hotmail.com>
2017-11-21 13:38:12 +0100

    Conflicts in the branch solved

--- alumni-brussels-le-wagon/app/helpers/batches_helper.rb
+++ alumni-brussels-le-wagon/app/helpers/batches_helper.rb
@@ -1,5 +1,4 @@
 module BatchesHelper
-
   # def batches_in_3_cols
   #   batches = data["batches"]
   #   batches_cols = [[], [], []]
@@ -50,5 +49,31 @@ module BatchesHelper
   #   sibling_student_id += step if sibling_student.fake
   #   return "\"#{sibling_student_id.to_s}\""
   # end
+
+  def batches_in_3_cols
+    batches = data["batches"]
+    batches_cols = [[], [], []]
+    batches.each_with_index do |batch, i|
+      batch_col = ( (i+1) % 3) - 1
+      batches_cols[batch_col] << batch
+    end
+    batches_cols
+  end
+
+  def numb_of_batches
+    data["batches"].length
+  end
+
+  def batch_id(num_col, num_row)
+    (num_col.to_i + 1) + (num_row.to_i)*3
+  end
+
+  def batch_gradient_color(index)
+    if index.even?
+      'rgba(44, 62, 80, 0.92), rgba(44, 62, 80, 0.9)'
+    else
+      'rgba(57, 68, 89, 0.92), rgba(57, 68, 89, 0.9)'
+    end
+  end
 
 end
